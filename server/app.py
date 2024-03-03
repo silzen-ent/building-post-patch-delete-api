1afa292045341fefaaf73c0b91af73d437110c36
Alan Silva <silzen.ent@gmail.com>
2024-03-03 01:39:35 -0700

    Completed Codealong: Building a POST, PATCH, & DELETE Api

--- server/app.py
+++ server/app.py
@@ -52,20 +52,42 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+# This route will allow us to see all reviews or create a new review
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            reviews,
+            200
+        )
+        return response
+    
+    # This conditional block will handle the POST request to create a new review
+    elif request.method == 'POST':
+        new_review = Review(
+            score=request.form.get("score"),
+            comment=request.form.get("comment"),
+            game_id=request.form.get("game_id"),
+            user_id=request.form.get("user_id"),
+        )
 
-    return response
+        db.session.add(new_review)
+        db.session.commit()
+
+        review_dict = new_review.to_dict()
+
+        response = make_response(
+            review_dict,
+            201
+        )
+        return response 
+
 
 @app.route('/users')
 def users():
@@ -81,6 +103,56 @@ def users():
     )
 
     return response
+
+
+# This route will allow us to see a review by its ID, update it, or delete a review by its ID
+@app.route('/reviews/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def review_by_id(id):
+    review = Review.query.filter_by(id=id).first()
+
+    # GET Request
+    if request.method == 'GET':
+        review_dict = review.to_dict()
+        response = make_response(
+            review_dict,
+            200
+        )
+        return response
+    
+    # PATCH Request
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(review, attr, request.form.get(attr))
+
+        db.session.add(review)
+        db.session.commit()
+
+        review_dict = review.to_dict()
+        response = make_response(
+            review_dict,
+            200
+        )
+        return response
+
+    # DELETE Request
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."
+        }
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
